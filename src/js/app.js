5ffa1a59622e6e95facd4ad98370de99623616d4
SergeyBildanov <troktrokov@gmail.com>
2024-07-16 17:26:45 +0400

    app.js changed

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 import CatchGoblin from "./catch-goblin"
 document.addEventListener("DOMContentLoaded", ()=>{
-  let game = new CatchGoblin(document.querySelector(".container"));
+  let container = document.querySelector(".container");
+  for(let i = 0; i < 16; i++){
+    let hole = document.createElement("div");
+    hole.classList.add("hole");
+    container.appendChild(hole);
+  }
+  let game = new CatchGoblin(container);
   game.addGoblin(Math.floor(Math.random() * 16));
   setInterval(()=>{
     game.moveGoblin();
